de2e2ffc4b99822a3ca595a879a775d9cbf25f20
mihirk <mihir.khatwani@gmail.com>
2018-04-05 13:26:38 +0530

    Adding parent organisation id

--- create_organisation.sql
+++ create_organisation.sql
@@ -8,5 +8,5 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO sewa_rural;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO sewa_rural;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO sewa_rural;
 
-INSERT INTO organisation (name, db_user, uuid)
-VALUES ('Sewa Rural', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5');
+INSERT INTO organisation (name, db_user, uuid, parent_organisation_id)
+VALUES ('Sewa Rural', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5', 1);
