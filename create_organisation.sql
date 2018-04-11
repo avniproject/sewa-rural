31ac24c0be9810759d4a4967e26865219f22f2ba
Vinay Venu <ijkvinay@gmail.com>
2018-04-11 13:21:46 +0530

    Remove hardcoding of organisation id

--- create_organisation.sql
+++ create_organisation.sql
@@ -8,5 +8,6 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO sewa_rural;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO sewa_rural;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO sewa_rural;
 
-INSERT INTO organisation (name, db_user, uuid, parent_organisation_id)
-VALUES ('Sewa Rural', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5', 1);
+
+INSERT into organisation(name, db_user, uuid, parent_organisation_id)
+    SELECT 'Sewa Rural', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5', id FROM organisation WHERE name = 'OpenCHS';
