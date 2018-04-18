1d8211bacd899e80a18a06421b70e783c44a1a81
mihirk <mihir.khatwani@gmail.com>
2018-04-18 14:10:11 +0530

    Posting to Sewa Rural Old

--- create_organisation.sql
+++ create_organisation.sql
@@ -10,4 +10,4 @@ GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO sewa_rural;
 
 
 INSERT into organisation(name, db_user, uuid, parent_organisation_id)
-    SELECT 'Sewa Rural', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5', id FROM organisation WHERE name = 'OpenCHS';
+    SELECT 'Sewa Rural Old', 'sewa_rural', 'b5399a48-60c4-4cea-b19e-d824c9dd00f5', id FROM organisation WHERE name = 'OpenCHS';
