1b768a1b9ffcceaf58c3196d608c0bf2add49096
Hiren Thacker <hithacker@gmail.com>
2019-06-14 12:21:37 +0530

    add a followup visit only when encounter type is annual visit

--- adolescent/visitSchedules.js
+++ adolescent/visitSchedules.js
@@ -1,10 +1,10 @@
 import {RuleFactory, VisitScheduleBuilder, RuleCondition} from "rules-config";
 import moment from "moment";
 
-const SickleCellFollowup = RuleFactory("92cd5f05-eec3-4e70-9537-62119c5e3a16", "VisitSchedule");
+const AnnualVisitSchedule = RuleFactory("92cd5f05-eec3-4e70-9537-62119c5e3a16", "VisitSchedule");
 
-@SickleCellFollowup("02c00bfd-2190-4d0a-8c1d-5d4596badc29", "Sickle Cell Followup", 100.0)
-class SickleCellFollowupScheduleSR {
+@AnnualVisitSchedule("02c00bfd-2190-4d0a-8c1d-5d4596badc29", "Sickle Cell Followup", 100.0)
+class AnnualVisitScheduleSR {
     static exec(programEncounter, visitSchedule = [], scheduleConfig) {
         const programEnrolment = programEncounter.programEnrolment;
         const scheduleBuilder = new VisitScheduleBuilder({
@@ -12,12 +12,14 @@ class SickleCellFollowupScheduleSR {
             programEnrolment: programEnrolment,
         });
 
-        scheduleBuilder.add({
-            name: "Sickle Cell Vulnerability Followup",
-            encounterType: "Sickle Cell Vulnerability",
-            earliestDate: moment().add(1, "month"),
-            maxDate: moment().add(1, "month").add(15, "days")
-        }).when.valueInEncounter("Sickling Test Result").containsAnswerConceptName("Disease");
+        if(programEncounter.encounterType.name === "Annual Visit") {
+            scheduleBuilder.add({
+                name: "Sickle Cell Vulnerability Followup",
+                encounterType: "Sickle Cell Vulnerability",
+                earliestDate: moment().add(1, "month"),
+                maxDate: moment().add(1, "month").add(15, "days")
+            }).when.valueInEncounter("Sickling Test Result").containsAnswerConceptName("Disease");
+        }
 
         return scheduleBuilder.getAllUnique("encounterType");
 
@@ -25,5 +27,5 @@ class SickleCellFollowupScheduleSR {
 }
 
 export {
-    SickleCellFollowupScheduleSR,
+    AnnualVisitScheduleSR,
 }
