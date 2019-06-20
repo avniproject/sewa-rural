6c4e94e9025cf6f0fef76c6ccd6f9339edf6906a
Vinod Deolal <vinoddeolal@gmail.com>
2019-06-20 15:06:03 +0530

    0000 | remove decisions and system and recomendations for annual visit

--- adolescent/rules/adolescentProgramEncounterDecision.js
+++ adolescent/rules/adolescentProgramEncounterDecision.js
@@ -1,17 +1,9 @@
-import * as LoadAndForget from '../formFilters/cancelFormFilters';
 import {FormElementsStatusHelper} from "rules-config/rules";
 import RoutineEncounterHandler from "../formFilters/RoutineEncounterHandler";
 import DropoutEncounterFormHandler from "../formFilters/DropoutEncounterFormHandler";
-import {encounterDecisions as vulnerabilityDecisionsFromEncounter} from '../decisions/vulnerabilityDecisions';
-import {encounterDecisions as counsellingEncounterDecisions} from '../decisions/counsellingDecisions';
 import {getNextScheduledVisits} from './adolescentVisitSchedule';
-import {referralDecisions} from "../decisions/referralDecision";
 import {RuleFactory} from 'rules-config/rules';
 import AnnualVisitHandler from "../formFilters/AnnualVisitHandler";
-
-const RoutineVisitDecisions = RuleFactory("35e54f14-3a23-45a3-b90e-5383fa026ffd", "Decision");
-const DropoutHomeVisit = RuleFactory("54636d6b-33bf-4faf-9397-eb3b1d9b1792", "Decision");
-const DropoutFollowUpVisit = RuleFactory("0c444bf3-54c3-41e4-8ca9-f0deb8760831", "Decision");
 
 const RoutineVisitViewFilters = RuleFactory("35e54f14-3a23-45a3-b90e-5383fa026ffd", "ViewFilter");
 const DropoutHomeViewFilters = RuleFactory("54636d6b-33bf-4faf-9397-eb3b1d9b1792", "ViewFilter");
@@ -25,13 +17,6 @@ const encounterTypeHandlerMap = new Map([
     ['Midline Visit', new RoutineEncounterHandler()],
     ['Dropout Home Visit', new DropoutEncounterFormHandler()]
 ]);
-
-
-const getDecisions = (programEncounter) => {
-    let vulnerabilityEncounterDecisions = vulnerabilityDecisionsFromEncounter(programEncounter.programEnrolment, programEncounter);
-    let counsellingDecisions = counsellingEncounterDecisions(vulnerabilityEncounterDecisions, programEncounter);
-    return referralDecisions(counsellingDecisions, programEncounter);
-};
 
 const getFormElementsStatuses = (programEncounter, formElementGroup) => {
     let handler = encounterTypeHandlerMap.get(programEncounter.encounterType.name);
@@ -59,26 +44,4 @@ class DFFilterHandler {
     }
 }
 
-
-@RoutineVisitDecisions("13a72ed7-d83a-4f7c-9f7a-58dad6f78986", "All Routine Visit Decisions", 1.0)
-class RoutineVisitDecisionHandler {
-    static exec(programEncounter) {
-        return getDecisions(programEncounter);
-    }
-}
-
-@DropoutHomeVisit("a99466c8-f6fa-4cf7-bd31-45f4bd76d9d7", "All Dropout Home Visit Decisions", 1.0)
-class DHVisitDecisionHandler {
-    static exec(programEncounter) {
-        return getDecisions(programEncounter);
-    }
-}
-
-@DropoutFollowUpVisit("a250245f-7e08-4275-aa47-5256ed268e03", "All Dropout Followup Visit Decisions", 1.0)
-class DFVisitDecisionHandler {
-    static exec(programEncounter) {
-        return getDecisions(programEncounter);
-    }
-}
-
-export {RoutineFilterHandler, DHFilterHandler, DFFilterHandler, RoutineVisitDecisionHandler, DHVisitDecisionHandler, DFVisitDecisionHandler};
+export {RoutineFilterHandler, DHFilterHandler, DFFilterHandler};
