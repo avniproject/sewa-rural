ebbb72c05e1664b2cd8f604df26588c45fe97262
Hiren Thacker <hithacker@gmail.com>
2019-07-17 12:15:25 +0530

    #22 | Add Anemia Status and Vulnerabilities decisions to be used in report filters

--- adolescent/formFilters/AnnualVisitDecisions.js
+++ adolescent/formFilters/AnnualVisitDecisions.js
@@ -13,25 +13,22 @@ const Decision = RuleFactory("35e54f14-3a23-45a3-b90e-5383fa026ffd", "Decision")
 
 @Decision("ee261afb-73ca-4ea2-8838-90dbd34a541e", "Annual Visit Decisions", 100.0, {})
 export class AnnualVisitDecisionHandler {
-    static referrals(programEncounter) {
-        const referralAdvice = new ComplicationsBuilder({
-            programEnrolment: programEncounter.programEnrolment,
+    static referToHospitalDecisions(programEncounter) {
+        const complicationsBuilder = new ComplicationsBuilder({
             programEncounter: programEncounter,
             complicationsConcept: "Refer to hospital immediately for"
         });
 
-        if (!programEncounter) return referralAdvice.getComplications();
-
-        referralAdvice
+        complicationsBuilder
             .addComplication("Severe Anemia")
             .when.valueInEncounter("Hb")
             .is.lessThanOrEqualTo(7);
-        referralAdvice
+        complicationsBuilder
             .addComplication("Sickle cell disease")
             .when.valueInEncounter("Sickling Test Result")
             .containsAnswerConceptName("Disease");
 
-        referralAdvice
+        complicationsBuilder
             .addComplication("Medical Problems")
             .when.valueInEncounter("Is there any physical defect?")
             .containsAnswerConceptName("Yes")
@@ -66,7 +63,7 @@ export class AnnualVisitDecisionHandler {
             .or.when.valueInEncounter("Is there any developmental delay or disability seen?")
             .containsAnswerConceptName("Yes");
 
-        referralAdvice
+        complicationsBuilder
             .addComplication("Menstrual disorders")
             .when.valueInEncounter("Menstrual disorders")
             .containsAnswerConceptNameOtherThan("No problem")
@@ -75,16 +72,16 @@ export class AnnualVisitDecisionHandler {
             .or.when.valueInEncounter("Does she remain absent during menstruation?")
             .containsAnswerConceptName("Yes");
 
-        referralAdvice
+        complicationsBuilder
             .addComplication("Additional Medical Problems")
             .when.valueInEncounter("Is there any other condition you want to mention about him/her?")
             .containsAnswerConceptNameOtherThan("No problem");
-        referralAdvice
+        complicationsBuilder
             .addComplication("Addiction (Self)")
             .when.valueInEncounter("Addiction Details")
             .containsAnswerConceptNameOtherThan("No Addiction");
 
-        referralAdvice
+        complicationsBuilder
             .addComplication("Sexual Problems")
             .when.valueInEncounter("Burning Micturition")
             .containsAnswerConceptName("Yes")
@@ -93,15 +90,98 @@ export class AnnualVisitDecisionHandler {
             .or.when.valueInEncounter("Yellowish discharge from Vagina / penis")
             .containsAnswerConceptName("Yes");
 
-        return referralAdvice.getComplications();
+        return complicationsBuilder.getComplications();
     }
 
+    static vulnerabilityDecisions(programEncounter) {
+        const complicationsBuilder = new ComplicationsBuilder({
+            programEncounter: programEncounter,
+            complicationsConcept: "Adolescent Vulnerabilities"
+        });
+
+        complicationsBuilder
+            .addComplication("Severe Anemia")
+            .when.valueInEncounter("Hb")
+            .is.lessThanOrEqualTo(7);
+
+        complicationsBuilder
+            .addComplication("Moderate Anemia")
+            .when.valueInEncounter("Hb")
+            .is.greaterThanOrEqualTo(7.1)
+            .and.valueInEncounter("Hb")
+            .is.lessThanOrEqualTo(10);
+
+        complicationsBuilder
+            .addComplication("Severe malnourishment")
+            .when.valueInEncounter("BMI")
+            .lessThanOrEqualTo(14.5);
+
+        complicationsBuilder
+            .addComplication("School dropout")
+            .when.valueInEncounter("School going")
+            .containsAnyAnswerConceptName("Dropped Out");
+
+        complicationsBuilder
+            .addComplication("Chronic Sickness")
+            .when.valueInEncounter("Is there any other condition you want to mention about him/her?")
+            .containsAnswerConceptNameOtherThan("No problem");
+
+        complicationsBuilder
+            .addComplication("Sickle cell disease")
+            .when.valueInEncounter("Sickling Test Result")
+            .containsAnswerConceptName("Disease");
+
+        complicationsBuilder
+            .addComplication("Menstrual disorders")
+            .when.valueInEncounter("Menstrual disorders")
+            .containsAnswerConceptNameOtherThan("No problem");
+
+        complicationsBuilder
+            .addComplication("Addiction (Self)")
+            .when.valueInEncounter("Addiction Details")
+            .containsAnswerConceptNameOtherThan("No Addiction");
+
+        return complicationsBuilder.getComplications();
+    }
+
+    static anemiaStatusDecisions(programEncounter) {
+        const complicationsBuilder = new ComplicationsBuilder({
+            programEncounter: programEncounter,
+            complicationsConcept: "Anemia Status"
+        });
+
+        complicationsBuilder
+            .addComplication("Severe")
+            .when.valueInEncounter("Hb")
+            .is.lessThanOrEqualTo(7);
+
+        complicationsBuilder
+            .addComplication("Moderate")
+            .when.valueInEncounter("Hb")
+            .is.greaterThanOrEqualTo(7.1)
+            .and.valueInEncounter("Hb")
+            .is.lessThanOrEqualTo(10);
+
+        complicationsBuilder
+            .addComplication("Mild")
+            .when.valueInEncounter("Hb")
+            .is.greaterThanOrEqualTo(10.1)
+            .and.valueInEncounter("Hb")
+            .is.lessThanOrEqualTo(11.9);
+
+        complicationsBuilder
+            .addComplication("Normal")
+            .when.valueInEncounter("Hb")
+            .is.greaterThanOrEqualTo(12);
+
+        return complicationsBuilder.getComplications();
+    }
+
+
     static exec(programEncounter, decisions, context, today) {
-        const recommendation = AnnualVisitDecisionHandler.referrals(programEncounter);
-        decisions["encounterDecisions"] = decisions["encounterDecisions"] || [];
-        decisions["encounterDecisions"] = decisions["encounterDecisions"]
-            .filter(d => d.name !== recommendation.name)
-            .concat(recommendation);
+        decisions.encounterDecisions.push(AnnualVisitDecisionHandler.referToHospitalDecisions(programEncounter));
+        decisions.encounterDecisions.push(AnnualVisitDecisionHandler.vulnerabilityDecisions(programEncounter));
+        decisions.encounterDecisions.push(AnnualVisitDecisionHandler.anemiaStatusDecisions(programEncounter));
         return decisions;
     }
 }
