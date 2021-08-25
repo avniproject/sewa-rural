b92eee87928de3543a3cd4776e8a3350e33c8b1f
Vinod Deolal <vinoddeolal@gmail.com>
2021-08-25 16:25:56 +0530

    #0 | improvements in the offline reports

--- OfflineReport.js
+++ OfflineReport.js
@@ -8,107 +8,86 @@
 //Adolescents having severe anemia
 'use strict';
 ({params, imports}) => {
-    const isSevereAnemia = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit", "Endline Visit");
-        const hb = latestVisit && latestVisit.getObservationReadableValue('Hb');
-        return hb ? hb <= 7 : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => isSevereAnemia(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Quarterly Visit' OR encounterType.name = 'Endline Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => enc.getObservationReadableValue('Hb') <= 7)
+        .map(enc => enc.programEnrolment.individual)
 };
 
 //Adolescents having moderate anemia
 'use strict';
 ({params, imports}) => {
-    const isModerateAnemia = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit", "Endline Visit");
-        const hb = latestVisit && latestVisit.getObservationReadableValue('Hb');
-        return hb ? (hb >= 7.1 && hb <= 10) : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => isModerateAnemia(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Quarterly Visit' OR encounterType.name = 'Endline Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => {
+            const hb = enc.getObservationReadableValue('Hb');
+            return hb >= 7.1 && hb <= 10;
+        })
+        .map(enc => enc.programEnrolment.individual)
 };
 
 
 //Adolescents having sickle disease
 'use strict';
 ({params, imports}) => {
-    const isSickleDisease = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit", "Endline Visit");
-        const result = latestVisit && latestVisit.getObservationReadableValue('Sickling Test Result');
-        return result ? result === 'Disease' : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => isSickleDisease(enrolment)))
+    return params.db.objects('ProgramEnrolment')
+        .filtered(`individual.voided = false AND voided = false AND program.name = 'Adolescent' AND programExitDateTime = null`)
+        .filtered('TRUEPREDICATE sort(individual.uuid asc , enrolmentDateTime desc) Distinct(individual.uuid)')
+        .filtered(`SUBQUERY(encounters, $encounter, $encounter.voided = false and SUBQUERY($encounter.observations, $observation, $observation.concept.uuid = 'b5daf90d-5b71-4b53-827f-edd4f6539d15' and ($observation.valueJSON contains '2c343c7a-db14-4531-902a-d7b169300073')).@count > 0).@count > 0`)
+        .map(enl => enl.individual)
 };
 
 //Adolescents absent due to menstrual disorders
 'use strict';
 ({params, imports}) => {
-    const isAbsentDueToMensDisorder = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit", "Endline Visit");
-        const isAbsent = latestVisit && latestVisit.getObservationReadableValue('Does she remain absent during menstruation?');
-        return isAbsent ? isAbsent === 'Yes' : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => isAbsentDueToMensDisorder(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Quarterly Visit' OR encounterType.name = 'Endline Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => enc.getObservationReadableValue('Does she remain absent during menstruation?') === 'Yes')
+        .map(enc => enc.programEnrolment.individual)
 };
 
 //Adolescents having chronic sickness
 'use strict';
 ({params, imports}) => {
-    const hasChronicSickness = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit");
-        const chronicSickness = latestVisit && latestVisit.getObservationReadableValue('Is there any other condition you want to mention about him/her?');
-        return chronicSickness ? !_.includes(chronicSickness, 'No problem') : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => hasChronicSickness(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => !_.includes(enc.getObservationReadableValue('Is there any other condition you want to mention about him/her?'), 'No problem'))
+        .map(enc => enc.programEnrolment.individual)
 };
 
 //Adolescents having severe malnutrition
 'use strict';
 ({params, imports}) => {
-    const hasSevereMalnutrition = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Endline Visit");
-        const bmi = latestVisit && latestVisit.getObservationReadableValue('BMI');
-        return bmi ? bmi < 18.5 : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => hasSevereMalnutrition(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Endline Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => enc.getObservationReadableValue('BMI') < 18.5)
+        .map(enc => enc.programEnrolment.individual)
 };
 
 //Adolescents who dropped out of school
 'use strict';
 ({params, imports}) => {
-    const isSchoolDropout = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit", "Endline Visit");
-        const schoolGoing = latestVisit && latestVisit.getObservationReadableValue('School going');
-        return schoolGoing ? schoolGoing === 'Dropped Out' : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => isSchoolDropout(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Quarterly Visit' OR encounterType.name = 'Endline Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => enc.getObservationReadableValue('School going') === 'Dropped Out')
+        .map(enc => enc.programEnrolment.individual)
 };
 
 
 //Adolescents having addiction
 'use strict';
 ({params, imports}) => {
-    const hasAddiction = (enrolment) => {
-        const latestVisit = enrolment.lastFulfilledEncounter("Annual Visit", "Quarterly Visit");
-        const addiction = latestVisit && latestVisit.getObservationReadableValue('Addiction Details');
-        return addiction ? _.includes(["Alcohol", "Tobacco", "Both"], addiction) : false;
-    };
-    return params.db.objects('Individual')
-        .filtered(`SUBQUERY(enrolments, $enrolment, $enrolment.program.name = 'Adolescent' and $enrolment.programExitDateTime = null and $enrolment.voided = false).@count > 0 and voided = false`)
-        .filter((individual) => _.some(individual.enrolments, (enrolment) => hasAddiction(enrolment)))
+    return params.db.objects('ProgramEncounter')
+        .filtered(`programEnrolment.individual.voided = false AND programEnrolment.voided = false AND programEnrolment.program.name = 'Adolescent' AND programEnrolment.programExitDateTime = null AND voided = false AND encounterDateTime <> null AND (encounterType.name = 'Annual Visit' OR encounterType.name = 'Quarterly Visit')`)
+        .filtered('TRUEPREDICATE sort(programEnrolment.individual.uuid asc , encounterDateTime desc) Distinct(programEnrolment.individual.uuid)')
+        .filter(enc => _.includes(["Alcohol", "Tobacco", "Both"],enc.getObservationReadableValue('Addiction Details')))
+        .map(enc => enc.programEnrolment.individual)
 };
 
 //Adolescents exited
